70c360d11978bbeebf1c45b39a5f7bf787865a98
Vinícius de Oliveira <vinicius.oliveira@warren.com.br>
2022-05-25 00:04:35 -0300

    refactor(CustomerValidator): validation for cpf, full name and email/whatsapp

--- Teste.API/Validator/CustomerValidator.cs
+++ Teste.API/Validator/CustomerValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Teste.API.Extensions;
 using Teste.API.Models;
 
 namespace Teste.API.Validator
@@ -7,51 +8,50 @@ namespace Teste.API.Validator
     {
         public CustomerValidator()
         {
+            RuleFor(customer => customer)
+                .Must(x => x.EmailSms || x.Whatsapp);
+
+            RuleFor(customer => customer.Id)
+                .Empty();
+
             RuleFor(customer => customer.FullName)
-                .NotNull();
+                .NotNull()
+                .Must(x => x.Split().Length >= 2);
+
             RuleFor(customer => customer.Email)
                 .EmailAddress();
+
             RuleFor(customer => customer.Cpf)
-                .MinimumLength(11)
-                .Must(CpfValido)
+                .Length(11)
+                .Matches("[0-9]{11}")
+                .WithMessage("Cpf deve conter apenas números!")
+                .Must(ValidateCpf)
                 .WithMessage("Cpf invalido!");
+
             RuleFor(customer => customer.Cellphone)
-                .NotNull();
+                .Must(x => !string.IsNullOrWhiteSpace(x));
+
             RuleFor(customer => customer.BirthDate)
-                .NotNull();
-            RuleFor(customer => customer.EmailSms)
-                .NotNull();
-            RuleFor(customer => customer.Whatsapp)
                 .NotNull();
         }
 
-        private bool CpfValido(string cpf)
+        private static bool ValidateCpf(string cpf)
         {
-            String[] digitos = new string[11];
-            for (var i = 0; i < 11; i++)
+            var firstDigitAfterDash = 0;
+            for (int i = 0; i < cpf.Length - 2; i++)
             {
-                digitos[i] = cpf.Substring(i, 1);
+                firstDigitAfterDash += cpf.ToIntAt(i) * (10 - i);
             }
-            int digito1 = 0;
-            for (int i = 0; i < 9; i++)
+            firstDigitAfterDash = (firstDigitAfterDash * 10) % 11;
+
+            var secondDigitAfterDash = 0;
+            for (int i = 0; i < cpf.Length - 1; i++)
             {
-                digito1 += int.Parse(digitos[i]) * (10 - i);
-                Console.WriteLine(digito1);
+                secondDigitAfterDash += cpf.ToIntAt(i) * (11 - i);
             }
-            digito1 *= 10;
-            digito1 %= 11;
-            int digito2 = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                digito2 += int.Parse(digitos[i]) * (11 - i);
-            }
-            digito2 *= 10;
-            digito2 %= 11;
-            if ((digito1 == int.Parse(digitos[9])) && (digito2 == int.Parse(digitos[10])))
-            {
-                return true;
-            }
-            else { return false; }
+            secondDigitAfterDash = (secondDigitAfterDash * 10) % 11;
+
+            return firstDigitAfterDash == cpf.ToIntAt(^2) && secondDigitAfterDash == cpf.ToIntAt(^1);
         }
     }
 }
